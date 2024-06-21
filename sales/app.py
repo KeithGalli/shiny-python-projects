317210736f875f6e6c9a91d69b9b3b620e9eeab2
Keith Galli <k.galli603@gmail.com>
2024-06-21 15:35:38 -0400

    Finished video #3 code

--- sales/app.py
+++ sales/app.py
@@ -7,15 +7,7 @@ from shiny import reactive
 from shiny.express import render, input, ui
 from shinywidgets import render_plotly
 
-ui.page_opts(title="Sales Dashboard - Video 2 of 5", fillable=False)
-
-ui.input_checkbox("bar_color", "Make Bars Red?", False)  
-
-ui.input_numeric("n", "Number of Items", 5, min=0, max=20)
-
-@reactive.calc
-def color():
-    return "red" if input.bar_color() else "blue"
+ui.page_opts(title="Sales Dashboard - Video 3 of 5", fillable=False)
 
 @reactive.calc
 def dat():
@@ -23,52 +15,95 @@ def dat():
     df = pd.read_csv(infile)
     df["order_date"] = pd.to_datetime(df["order_date"])
     df["month"] = df["order_date"].dt.month_name()
+    df['value'] = df['quantity_ordered'] * df['price_each']
     return df
 
-@render_plotly
-def plot1():
-    df = dat()
-    top_sales = df.groupby('product')['quantity_ordered'].sum().nlargest(input.n()).reset_index()
-    fig = px.bar(top_sales, x='product', y='quantity_ordered')
-    fig.update_traces(marker_color=color())
-    return fig
+with ui.card():  
+    ui.card_header("Sales by City 2023")
 
-ui.input_selectize(
-    "city",
-    "Select a City:",
-    [
-        "Dallas (TX)",
-        "Boston (MA)",
-        "Los Angeles (CA)",
-        "San Francisco (CA)",
-        "Seattle (WA)",
-        "Atlanta (GA)",
-        "New York City (NY)",
-        "Portland (OR)",
-        "Austin (TX)",
-        "Portland (ME)",
-    ],
-    multiple=False,
-    selected='Boston (MA)'
-)
+    with ui.layout_sidebar():  
+        with ui.sidebar(bg="#f8f8f8", open='open'):  
+            ui.input_selectize(
+                "city",
+                "Select a City:",
+                [
+                    "Dallas (TX)",
+                    "Boston (MA)",
+                    "Los Angeles (CA)",
+                    "San Francisco (CA)",
+                    "Seattle (WA)",
+                    "Atlanta (GA)",
+                    "New York City (NY)",
+                    "Portland (OR)",
+                    "Austin (TX)",
+                    "Portland (ME)",
+                ],
+                multiple=False,
+                selected='Boston (MA)'
+            )
+
+        @render_plotly
+        def sales_over_time():
+            df = dat()
+            print(list(df.city.unique()))
+            sales = df.groupby(["city", "month"])["quantity_ordered"].sum().reset_index()
+            sales_by_city = sales[sales["city"] == input.city()]
+            month_orders = calendar.month_name[1:]
+            fig = px.bar(
+                sales_by_city,
+                x="month",
+                y="quantity_ordered",
+                title=f"Sales over Time -- {input.city()}",
+                category_orders={"month": month_orders},
+            )
+            return fig
+
+with ui.layout_column_wrap(width=1/2):
+    with ui.navset_card_underline(id="tab", footer=ui.input_numeric("n", "Number of Items", 5, min=0, max=20)):  
+        with ui.nav_panel("Top Sellers"):
+
+            @render_plotly
+            def plot_top_sellers():
+                df = dat()
+                top_sales = df.groupby('product')['quantity_ordered'].sum().nlargest(input.n()).reset_index()
+                fig = px.bar(top_sales, x='product', y='quantity_ordered')
+                return fig
+
+        with ui.nav_panel("Top Sellers Value ($)"):
+
+            @render_plotly
+            def plot_top_sellers_value():
+                df = dat()
+                top_sales = df.groupby('product')['value'].sum().nlargest(input.n()).reset_index()
+                fig = px.bar(top_sales, x='product', y='value')
+                return fig
+
+        with ui.nav_panel("Lowest Sellers"):
+
+            @render_plotly
+            def plot_lowest_sellers():
+                df = dat()
+                top_sales = df.groupby('product')['quantity_ordered'].sum().nsmallest(input.n()).reset_index()
+                fig = px.bar(top_sales, x='product', y='quantity_ordered')
+                return fig
+
+        with ui.nav_panel("Lowest Sellers Value ($)"):
+            @render_plotly
+            def plot_lowest_sellers_value():
+                df = dat()
+                top_sales = df.groupby('product')['value'].sum().nsmallest(input.n()).reset_index()
+                fig = px.bar(top_sales, x='product', y='value')
+                return fig
 
 
-@render_plotly
-def sales_over_time():
-    df = dat()
-    print(list(df.city.unique()))
-    sales = df.groupby(["city", "month"])["quantity_ordered"].sum().reset_index()
-    sales_by_city = sales[sales["city"] == input.city()]
-    month_orders = calendar.month_name[1:]
-    fig = px.bar(
-        sales_by_city,
-        x="month",
-        y="quantity_ordered",
-        title=f"Sales over Time -- {input.city()}",
-        category_orders={"month": month_orders},
-    )
-    fig.update_traces(marker_color=color())
-    return fig
+    with ui.card():
+        ui.card_header("Sales by Time of Day Heatmap")
+        "Heatmap"
+
+with ui.card():
+    ui.card_header("Sales by Location Map")
+    "Content Here"
+    
 
 with ui.card():
     ui.card_header("Sample Sales Data")
